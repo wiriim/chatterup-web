17e1812b2484042d38ed71dfa88b15c5e138036c
William Halim <51820876+wiriim@users.noreply.github.com>
2026-07-31 16:58:55 +0700

    fix: fix auth0 redirectUri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   auth0: {
     domain: 'dev-rzpz6syntjae61ts.us.auth0.com',
     clientId: 'OVe9oC7m5x6ZFMcuw5R1g04sYhNgVdWI',
-    redirectUri: 'chatter-up.win'
+    redirectUri: 'https://chatter-up.win'
   },
   backend: {
     fullUrl: '/api',
