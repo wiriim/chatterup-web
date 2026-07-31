2f5b5531bbcf7bffc5faf6fadf04483fca083928
William Halim <51820876+wiriim@users.noreply.github.com>
2026-07-31 16:23:25 +0700

    fix: remove environments dir from gitignore & dockerignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    auth0: {
+      domain: 'dev-rzpz6syntjae61ts.us.auth0.com',
+      clientId: 'OVe9oC7m5x6ZFMcuw5R1g04sYhNgVdWI',
+      redirectUri: 'http://localhost:4200'
+    },
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  auth0: {
+    domain: 'dev-rzpz6syntjae61ts.us.auth0.com',
+    clientId: 'OVe9oC7m5x6ZFMcuw5R1g04sYhNgVdWI',
+    redirectUri: 'http://localhost'
+  },
+  backend: {
+    fullUrl: '/api',
+    url: 'localhost'
+  }
+};
